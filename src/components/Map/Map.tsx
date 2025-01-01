bde3ca49825bec991ddac4105577ca10c97da23e
stephen <stephen@motts.id.au>
2025-01-01 20:11:00 +0800

    initial adding of noise layers

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,9 +1,9 @@
-import {MapContainer, Polygon, Popup, TileLayer} from "react-leaflet";
+import {LayerGroup, LayersControl, MapContainer, Polygon, Popup, TileLayer, WMSTileLayer} from "react-leaflet";
 
 import poly from '../../assets/poly.json'
 import {Fragment} from "react";
 
-const addGeoData = (props:MapProps)=> {
+const addGeoData = (props: MapProps) => {
     return (
         <Fragment>
             {
@@ -28,7 +28,7 @@ const addGeoData = (props:MapProps)=> {
 
 interface MapProps {
     filterVal: number,
-    filterCB:(value: number[]) => void
+    filterCB: (value: number[]) => void
 }
 
 const Map = (props: MapProps) => {
@@ -37,16 +37,51 @@ const Map = (props: MapProps) => {
         <div className={"h-screen"}>
 
             <MapContainer center={[-31.9514, 115.8617]} zoom={13} scrollWheelZoom={true} height={500}>
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                {addGeoData(props)}
+                <LayersControl position="topright">
+                    <LayersControl.Overlay checked name="Map">
+                        <LayerGroup>
+                            <TileLayer
+                                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+                            </LayerGroup>
+                    </LayersControl.Overlay>
+
+                                <LayersControl.Overlay name="Distance to Perth">
+                    <LayerGroup>
+                        {addGeoData(props)}
+                    </LayerGroup>
+                </LayersControl.Overlay>
+
                 {/*<Polygon positions={poly.features[0].geometry.coordinates} />*/}
                 {/*<GeoJSON data={poly}></GeoJSON>*/}
-            </MapContainer>
-        </div>
-    )
+                {/*https://catalogue.data.wa.gov.au/dataset/state-planning-policy-5-1-land-use-planning-in-the-vicinity-of-perth-airport*/}
+
+                <LayersControl.Overlay name="Jandacot Airport Noise">
+                    <LayerGroup>
+                        <WMSTileLayer
+                            url={"https://public-services.slip.wa.gov.au/public/services/SLIP_Public_Services/Property_and_Planning/MapServer/WMSServer"}
+                            params={{layers: 56, opacity: 0.3}}/>
+                    </LayerGroup>
+                </LayersControl.Overlay>
+                    <LayersControl.Overlay name="Perth Airport Noise">
+                    <LayerGroup>
+                        <WMSTileLayer
+                            url={"https://public-services.slip.wa.gov.au/public/services/SLIP_Public_Services/Property_and_Planning/MapServer/WMSServer"}
+                            params={{layers: 57, opacity: 0.3}}/>
+                    </LayerGroup>
+                </LayersControl.Overlay>
+                    <LayersControl.Overlay name="Rail and Road Noise">
+                    <LayerGroup>
+                        <WMSTileLayer
+                            url={"https://public-services.slip.wa.gov.au/public/services/SLIP_Public_Services/Property_and_Planning/MapServer/WMSServer"}
+                            params={{layers: 99, opacity: 0.3}}/>
+                    </LayerGroup>
+                </LayersControl.Overlay>
+                {/*<WMSTileLayer url={"https://public-services.slip.wa.gov.au/public/services/SLIP_Public_Services/Property_and_Planning/MapServer/WMSServer"} params={{layers:78}}/>*/}
+            </LayersControl>
+        </MapContainer>
+</div>
+)
 
 
 }
