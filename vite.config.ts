db14cf81c20c5a98fee9895550c49bbc279cc768
Stephen Mott <stephen@motts.id.au>
2025-04-20 02:48:05 +0800

    initial testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from 'tailwindcss'
+import { configDefaults } from 'vitest/config'
+
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
@@ -8,4 +10,12 @@ export default defineConfig({
         postcss: {
             plugins: [tailwindcss()],
         },
-    }})
+    },
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: ['./src/test/setup.ts'],
+        exclude: [...configDefaults.exclude, 'e2e/*'],
+        css: false
+    }
+})
